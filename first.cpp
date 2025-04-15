87a8f8253596f3e31f95566d588b642763e0e751
Ronniel Marange <sosarony675@gmail.com>
2025-04-15 15:57:04 -0400

    first commit

---
+++ first.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  const double Car_payment = 242.00;
+  const double Geico = 120.00;
+  const double Spectrum = 50.00;
+  const double Rent = 1250.00;
+  const double CC_BofA = 100.00;
+  const double Discover = 100.00;
+  const double CC_APPL =  100.00;
+  const double Anytime_Fitness = 40.00; 
+  const double AMZ_Prime = 15.00;
+  const double Spotify = 10.00;
+  const double Netflix = 15.00;
+  const double FPL =100.00;
+    return 0;
+}
